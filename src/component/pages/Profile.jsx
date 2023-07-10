3eb466c59958daff4e87a3622bd51dad77111e13
AbhinasDash <abhinasdash143@gmail.com>
2023-07-10 18:18:23 +0530

    Profil photo upload

--- src/component/pages/Profile.jsx
+++ src/component/pages/Profile.jsx
@@ -1,16 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import img from "../../user.png";
 import Camera from "../Icons/Camera";
+import { auth, db, storage } from "../../firebase";
+import {
+  ref,
+  getDownloadURL,
+  uploadBytes,
+  deleteObject,
+} from "firebase/storage";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 const Profile = () => {
   const [photo, setPhoto] = useState("");
+  const [user, setUser] = useState("");
 
-  console.log(photo);
-  return (
+  useEffect(() => {
+    getDoc(doc(db, "users", auth.currentUser.uid)).then((docs) => {
+      if (docs.exists) {
+        setUser(docs.data());
+      }
+    });
+
+    if (photo) {
+      const uploadPhoto = async () => {
+        const photoRef = ref(
+          storage,
+          `user/${new Date().getTime()}- ${photo.name}`
+        );
+        try {
+          if (user.imgPath) {
+            await deleteObject(ref(storage, user.imgPath));
+          }
+          const upload = await uploadBytes(photoRef, photo);
+          const url = await getDownloadURL(ref(storage, upload.ref.fullPath));
+
+          await updateDoc(doc(db, "users", auth.currentUser.uid), {
+            img: url,
+            imgPath: upload.ref.fullPath,
+          });
+          console.log(url);
+          setPhoto("");
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
+      uploadPhoto();
+    }
+  }, [photo]);
+
+  return user ? (
     <div className="card">
       <div className="Profile">
         <div className="image">
-          <img src={img} alt="User" />
+          <img src={user.img || img} alt="User" />
           <div className="overlay">
             <>
               <label htmlFor="photo">
@@ -25,14 +67,16 @@ const Profile = () => {
             </>
           </div>
         </div>
-        <div className="text-container">
-          <h3>Abhinas Dash</h3>
-          <p>abhinasdash143@gmail.com</p>
+        <div>
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
           <hr />
-          <small>Joining on 25 may</small>
+          <small>Joining on : {user.createdOn.toDate().toDateString()}</small>
         </div>
       </div>
     </div>
+  ) : (
+    "Loading your profile..."
   );
 };
 
